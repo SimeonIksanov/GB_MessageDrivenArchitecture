a754ed7633e9cc0224c95f56da1475fa2b7a4a58
Simeon Iksanov <simeon.iksanov@gmail.com>
2022-10-16 19:53:44 +0500

    HW02: problem 1

---
+++ src/Messaging/Producer.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace Messaging;
+
+public class Producer
+{
+    private readonly string _queueName;
+    private readonly string _hostname;
+
+
+    public Producer(string hostName, string queueName)
+    {
+        _hostname = hostName;
+        _queueName = queueName;
+    }
+
+
+    public void Send(string message)
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = _hostname,
+            Port = 5672,
+            UserName = "ssxaqqgm",
+            VirtualHost = "ssxaqqgm",
+            Password = "***"
+
+        };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.ExchangeDeclare(
+            exchange: "direct_exchange",
+            type: "direct",
+            durable: false,
+            autoDelete: false,
+            arguments: null);
+
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(
+            exchange: "direct_exchange",
+            routingKey: _queueName,
+            basicProperties: null,
+            body: body);
+    }
+}
---
+++ src/Restaurant.Booking/Services/Implementation/RabbitMQNotifier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Messaging;
+
+namespace Restaurant.Booking.Services.Implementation
+{
+    public class RabbitMQNotifier : INotifier
+    {
+        private readonly Producer _producer;
+
+        public RabbitMQNotifier(string hostName, string queueName)
+        {
+            _producer = new Producer(hostName, queueName);
+        }
+
+        public void NotifyAsync(string message)
+        {
+            _producer.Send(message);
+        }
+    }
+}
+
