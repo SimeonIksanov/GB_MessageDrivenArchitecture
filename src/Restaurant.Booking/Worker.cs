33133890c58e7c57a6a6e80ba3ccda2996ee98b6
Simeon Iksanov <simeon.iksanov@gmail.com>
2022-10-23 13:13:28 +0500

    HW03: problem 3 fix

--- src/Restaurant.Booking/Worker.cs
+++ src/Restaurant.Booking/Worker.cs
@@ -66,35 +66,44 @@ internal class Worker : BackgroundService
 internal class CircuitBreaker
 {
     private CircuitBreakerState _state = CircuitBreakerState.Closed;
-    Queue<bool> _queue = new Queue<bool>(30);
+    private Queue<bool> _queue = new Queue<bool>();
+    private int _queueLen = 30;
+    private int _maxFails = 10;
+    private int _maxTimeout = 1500;
+    private int _openPeriodInSeconds = 10;
+    private DateTime _openedAt;
+    private Stopwatch _stopWatch = new Stopwatch();
 
     public void Invoke(Action action)
     {
-        var stopWatch = new Stopwatch();
-
+        if (_state == CircuitBreakerState.Open && (DateTime.Now - _openedAt).TotalSeconds > _openPeriodInSeconds)
+        {
+            Console.WriteLine("new CircuitBreaker state HalfOpen");
+            _state = CircuitBreakerState.HalfOpen;
+        }
         if (_state == CircuitBreakerState.Open)
         {
             Console.WriteLine("CircuitBreaker Open");
             throw new Exception("CircuitBreaker Open");
         }
 
-        stopWatch.Start();
+        _stopWatch.Start();
         action.Invoke();
-        stopWatch.Stop();
+        _stopWatch.Stop();
 
-        _queue.Enqueue(stopWatch.ElapsedMilliseconds > 1500);
-        if (_queue.Count > 30)
+        _queue.Enqueue(_stopWatch.ElapsedMilliseconds > _maxTimeout);
+        if (_queue.Count > _queueLen)
         {
             _ = _queue.Dequeue();
-            if (_queue.Count(el => el == true) > 10)
+            if (_queue.Count(el => el == true) > _maxFails)
             {
                 _state = CircuitBreakerState.Open;
+                _openedAt = DateTime.Now;
                 Console.WriteLine("new CircuitBreaker state Open");
             }
             else
             {
                 _state = CircuitBreakerState.Closed;
-                Console.WriteLine("new CircuitBreaker state Closed");
             }
         }
     }
